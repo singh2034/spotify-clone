cb62c15510a9bd26b4e35d56e5fd03f4328b590d
Aayush <singhaayush2034@gmail.com>
2024-07-15 13:48:12 +0530

    updated

---
+++ src/components/DisplayAlbum.jsx
@@ -0,0 +1,71 @@
+import { useParams } from "react-router-dom";
+import Navbar from "./Navbar";
+import { albumsData, assets, songsData } from "../assets/assets";
+
+const DisplayAlbum = () => {
+  const { id } = useParams();
+  const albumData = albumsData[id];
+  return (
+    <>
+      <Navbar />
+      <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
+        <img
+          className="w-48 rounded"
+          src={albumData.image}
+          alt="album-data-image"
+        />
+        <div className="flex flex-col">
+          <p>Playlist</p>
+          <h2 className="text-5xl font-bold mb-4 md:text-7xl">
+            {albumData.name}
+          </h2>
+          <h4>{albumData.desc}</h4>
+          <p className="mt-1">
+            <img
+              src={assets.spotify_logo}
+              alt="spotify-logo"
+              className="inline-block w-5"
+            />
+            <b className="mx-2">Spotify</b>
+            &#x2022; 5,13,23,125 likes &#x2022; <b>50 songs,</b>
+            about 2 hours 30 min
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
+        <p>
+          <b className="mt-4">#</b>Title
+        </p>
+        <p>Album</p>
+        <p className="hidden sm:block">Date Added</p>
+        <img
+          src={assets.clock_icon}
+          alt="clock-icon-assets"
+          className="m-auto w-4 "
+        />
+      </div>
+      <hr />
+      {songsData.map((item, index) => (
+        <div
+          key={item.id}
+          className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
+        >
+          <p className="text-white">
+            <b className="mr-4 text-[#a7a7a7]"> {index + 1}</b>
+            <img
+              src={item.image}
+              alt="items-images"
+              className="inline w-10 mr-5"
+            />
+            {item.name}
+          </p>
+          <p className="text-[15px]">{albumData.name}</p>
+          <p className="text-[15px] hidden sm:block">5 days ago</p>
+          <p className="text-[15px] text-center">{item.duration}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default DisplayAlbum;
--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,41 @@
+import { albumsData, songsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
 import Navbar from "./Navbar";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today&apos;s Biggest Hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
